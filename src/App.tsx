ff93c9444fc64eb973676a49ae296f95d7c82df3
Frieder Bluemle <frieder.bluemle@gmail.com>
2021-09-23 01:24:53 +0200

    Update main app screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,67 @@
 import {StatusBar} from 'expo-status-bar';
-import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import React, {useState} from 'react';
+import {
+  Modal,
+  Pressable,
+  ScrollView,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+import Constants from 'expo-constants';
 
 export default function App() {
+  const [modalText, setModalText] = useState('');
+  const [modalVisible, setModalVisible] = useState(false);
+
+  const showModal = (text: string) => {
+    setModalText(text);
+    setModalVisible(true);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Modal
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => {
+          setModalVisible(!modalVisible);
+        }}>
+        <View style={styles.centeredView}>
+          <View style={styles.modalView}>
+            <ScrollView>
+              <Text style={styles.modalText}>{modalText}</Text>
+            </ScrollView>
+            <Pressable
+              style={[styles.button]}
+              onPress={() => setModalVisible(!modalVisible)}>
+              <Text style={styles.textStyle}>Close</Text>
+            </Pressable>
+          </View>
+        </View>
+      </Modal>
+      <View>
+        <Pressable
+          style={[styles.button]}
+          onPress={() => showModal(JSON.stringify(process.env, null, 2))}>
+          <Text style={styles.textStyle}>Show env</Text>
+        </Pressable>
+        <Pressable
+          style={[styles.button]}
+          onPress={() =>
+            showModal(JSON.stringify(Constants.manifest, null, 2))
+          }>
+          <Text style={styles.textStyle}>Show manifest</Text>
+        </Pressable>
+        <Pressable
+          style={[styles.button]}
+          onPress={() =>
+            showModal(JSON.stringify(Constants.manifest?.extra, null, 2))
+          }>
+          <Text style={styles.textStyle}>Show manifest.extra</Text>
+        </Pressable>
+      </View>
+
       <StatusBar style="auto" />
     </View>
   );
@@ -17,5 +73,40 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  centeredView: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  modalView: {
+    margin: 16,
+    backgroundColor: 'white',
+    padding: 8,
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  button: {
+    borderRadius: 4,
+    padding: 10,
+    margin: 4,
+    elevation: 2,
+    backgroundColor: '#2196F3',
+  },
+  textStyle: {
+    color: 'white',
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  modalText: {
+    fontFamily: 'monospace',
+    marginBottom: 8,
   },
 });
